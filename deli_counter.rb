64707c735cba74b2210f4461bf11c05f5066bbd9
Youngmin Park <codepark2019@gmail.com>
2020-05-30 04:01:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,16 @@
 require 'pry'
 
 def line(array)
-  binding.pry
+  # binding.pry
   # each name is in the array
   # the names are listed starting from 1
 
   new_modified_array = array.map.with_index(1) do |name, index|
     "#{index}. #{name}"
   end
-  puts "The line is currently: #{new_modified_array.join(" ")}"
-  puts "The line is currently empty."
-end
+
+  if array.length > 0
+    puts "The line is currently: #{new_modified_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
