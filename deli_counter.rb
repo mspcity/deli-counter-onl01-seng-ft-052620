e22b88d4a9b43ec6a2392696859ee94ec4cc0fd4
Youngmin Park <codepark2019@gmail.com>
2020-05-30 03:45:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,11 @@ require 'pry'
 
 def line(array)
   binding.pry
+  # each name is in the array
+  # the names are listed starting from 1
+
+  new_modified_array = array.map do |name| 
+  end
   puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   puts "The line is currently empty."
 end
