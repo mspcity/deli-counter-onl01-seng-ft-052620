a494ad57a87eac4a997d11f53a523789a98fe667
Youngmin Park <codepark2019@gmail.com>
2020-05-30 04:24:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+require 'pry'
+
+def line(array)
+  # binding.pry
+  # each name is in the array
+  # the names are listed starting from 1
+
+  new_modified_array = array.map.with_index(1) do |name, index|
+    "#{index}. #{name}"
+  end
+
+  if array.length > 0
+    puts "The line is currently: #{new_modified_array.join(" ")}"
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(array, name)
+  # binding.pry
+  array << name
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length > 0
+    puts "Currently serving #{array.shift}."
+  else
+    puts "There is nobody waiting to be served!"
+  end
+end
