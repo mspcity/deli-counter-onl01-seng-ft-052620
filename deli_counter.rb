368b3c9476a5b5b8c2280e4d98ad2c70dafb4a2c
Youngmin Park <codepark2019@gmail.com>
2020-05-30 03:49:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(array)
   # each name is in the array
   # the names are listed starting from 1
 
-  new_modified_array = array.map do |name|
+  new_modified_array = array.map.with_index(1) do |name, index|
+    "#{index}. #{name}"
   end
   puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
   puts "The line is currently empty."
